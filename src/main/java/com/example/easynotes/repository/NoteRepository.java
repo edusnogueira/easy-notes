5b8174db1b8d79860460c1461d9f0ca2e32d57ac
edusnogueira <edus.nogueira@gmail.com>
2018-03-21 14:13:45 -0300

    Add files via upload

---
+++ src/main/java/com/example/easynotes/repository/NoteRepository.java
@@ -0,0 +1,10 @@
+package com.example.easynotes.repository;
+
+import com.example.easynotes.model.Note;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+@Repository
+public interface NoteRepository extends JpaRepository<Note, Long> {
+
+}
